565ffdb859fe19bb9505c18522cddd3e98e6aa6b
JesusUribe3 <chucho.jo33@gmail.com>
2022-03-23 03:00:40 -0500

    Se agregan forms nuevos con nuevos comandos

--- frmprincipal.Designer.cs
+++ frmprincipal.Designer.cs
@@ -31,8 +31,11 @@ namespace Sistema_de_Mercado
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmprincipal));
             this.pnllogin = new System.Windows.Forms.Panel();
+            this.btnprincrear = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btncrear = new System.Windows.Forms.Button();
+            this.btnprineditar = new System.Windows.Forms.Button();
+            this.btnprineliminar = new System.Windows.Forms.Button();
+            this.btnprinsalir = new System.Windows.Forms.Button();
             this.pnllogin.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -40,13 +43,31 @@ namespace Sistema_de_Mercado
             // pnllogin
             // 
             this.pnllogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(200)))));
-            this.pnllogin.Controls.Add(this.btncrear);
+            this.pnllogin.Controls.Add(this.btnprinsalir);
+            this.pnllogin.Controls.Add(this.btnprineliminar);
+            this.pnllogin.Controls.Add(this.btnprineditar);
+            this.pnllogin.Controls.Add(this.btnprincrear);
             this.pnllogin.Controls.Add(this.pictureBox1);
             this.pnllogin.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnllogin.Location = new System.Drawing.Point(0, 0);
             this.pnllogin.Name = "pnllogin";
             this.pnllogin.Size = new System.Drawing.Size(266, 510);
             this.pnllogin.TabIndex = 2;
+            // 
+            // btnprincrear
+            // 
+            this.btnprincrear.FlatAppearance.BorderSize = 0;
+            this.btnprincrear.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnprincrear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnprincrear.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnprincrear.Image = ((System.Drawing.Image)(resources.GetObject("btnprincrear.Image")));
+            this.btnprincrear.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnprincrear.Location = new System.Drawing.Point(-3, 148);
+            this.btnprincrear.Name = "btnprincrear";
+            this.btnprincrear.Size = new System.Drawing.Size(266, 74);
+            this.btnprincrear.TabIndex = 3;
+            this.btnprincrear.Text = "Crear";
+            this.btnprincrear.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
@@ -60,15 +81,51 @@ namespace Sistema_de_Mercado
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // btncrear
+            // btnprineditar
             // 
-            this.btncrear.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btncrear.Location = new System.Drawing.Point(26, 180);
-            this.btncrear.Name = "btncrear";
-            this.btncrear.Size = new System.Drawing.Size(220, 133);
-            this.btncrear.TabIndex = 3;
-            this.btncrear.Text = "button1";
-            this.btncrear.UseVisualStyleBackColor = true;
+            this.btnprineditar.FlatAppearance.BorderSize = 0;
+            this.btnprineditar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnprineditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnprineditar.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnprineditar.Image = ((System.Drawing.Image)(resources.GetObject("btnprineditar.Image")));
+            this.btnprineditar.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnprineditar.Location = new System.Drawing.Point(-3, 218);
+            this.btnprineditar.Name = "btnprineditar";
+            this.btnprineditar.Size = new System.Drawing.Size(266, 74);
+            this.btnprineditar.TabIndex = 4;
+            this.btnprineditar.Text = "Editar";
+            this.btnprineditar.UseVisualStyleBackColor = true;
+            // 
+            // btnprineliminar
+            // 
+            this.btnprineliminar.FlatAppearance.BorderSize = 0;
+            this.btnprineliminar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnprineliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnprineliminar.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnprineliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnprineliminar.Image")));
+            this.btnprineliminar.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnprineliminar.Location = new System.Drawing.Point(0, 298);
+            this.btnprineliminar.Name = "btnprineliminar";
+            this.btnprineliminar.Size = new System.Drawing.Size(266, 74);
+            this.btnprineliminar.TabIndex = 5;
+            this.btnprineliminar.Text = "Eliminar";
+            this.btnprineliminar.UseVisualStyleBackColor = true;
+            this.btnprineliminar.Click += new System.EventHandler(this.btnprineliminar_Click);
+            // 
+            // btnprinsalir
+            // 
+            this.btnprinsalir.FlatAppearance.BorderSize = 0;
+            this.btnprinsalir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnprinsalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnprinsalir.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnprinsalir.Image = ((System.Drawing.Image)(resources.GetObject("btnprinsalir.Image")));
+            this.btnprinsalir.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnprinsalir.Location = new System.Drawing.Point(0, 424);
+            this.btnprinsalir.Name = "btnprinsalir";
+            this.btnprinsalir.Size = new System.Drawing.Size(266, 74);
+            this.btnprinsalir.TabIndex = 6;
+            this.btnprinsalir.Text = "Cerrar";
+            this.btnprinsalir.UseVisualStyleBackColor = true;
             // 
             // frmprincipal
             // 
@@ -87,7 +144,10 @@ namespace Sistema_de_Mercado
         #endregion
 
         private System.Windows.Forms.Panel pnllogin;
-        private System.Windows.Forms.Button btncrear;
+        private System.Windows.Forms.Button btnprincrear;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnprinsalir;
+        private System.Windows.Forms.Button btnprineliminar;
+        private System.Windows.Forms.Button btnprineditar;
     }
 }
--- frmprincipal.cs
+++ frmprincipal.cs
@@ -16,5 +16,10 @@ namespace Sistema_de_Mercado
         {
             InitializeComponent();
         }
+
+        private void btnprineliminar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
